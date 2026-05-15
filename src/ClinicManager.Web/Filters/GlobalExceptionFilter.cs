24f5c02d74a2189a08e502f496d09254cc12addc
Bartek <132946708+Bar-t-e-k@users.noreply.github.com>
2026-05-15 14:57:39 +0200

    Dodałem logowanie NLog i globalną obsługę wyjątków.
    NLog do strukturalnego logowania, nowy plik nlog.config. Zaktualizowałem plik Program.cs do konfiguracji i wyłączania logera.
    Dodałem globalny filtr wyjątków dla ujednoliconej obsługi błędów w żądaniach API i MVC.
    Dodałem do DataSeeder logowanie z wstrzykiwaniem zależności i obsługę błędów.

---
+++ src/ClinicManager.Web/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ClinicManager.Web.Filters
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<GlobalExceptionFilter> _logger;
+
+        public GlobalExceptionFilter(ILogger<GlobalExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            _logger.LogError(context.Exception, "Nieobsłużony wyjątek: {Message}", context.Exception.Message);
+
+            var isApiRequest = context.HttpContext.Request.Path.StartsWithSegments("/api");
+
+            if (isApiRequest)
+            {
+                context.Result = new ObjectResult(new { error = "Wystąpił wewnętrzny błąd serwera." })
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+            else
+            {
+                context.Result = new ViewResult { ViewName = "Error" };
+                context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            }
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
--- src/ClinicManager.Web/Program.cs
+++ src/ClinicManager.Web/Program.cs
@@ -1,71 +1,98 @@
 using ClinicManager.Web.Data;
+using ClinicManager.Web.Filters;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("Aplikacja startuje...");
 
-// 1. Rejestracja kontekstu bazy danych z użyciem SQL Server
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ClinicDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-// 2. Rejestracja systemu Identity
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
-    {
-        // Opcjonalne ułatwienie na czas dewelopmentu
-        options.SignIn.RequireConfirmedAccount = false;
-        options.Password.RequireDigit = false;
-        options.Password.RequiredLength = 4;
-        options.Password.RequireNonAlphanumeric = false;
-        options.Password.RequireUppercase = false;
-    })
-    .AddEntityFrameworkStores<ClinicDbContext>()
-    .AddDefaultUI()
-    .AddDefaultTokenProviders();
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // 1. Rejestracja kontekstu bazy danych z użyciem SQL Server
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<ClinicDbContext>(options =>
+        options.UseSqlServer(connectionString));
+
+    // 2. Rejestracja systemu Identity
+    builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        {
+            // Opcjonalne ułatwienie na czas dewelopmentu
+            options.SignIn.RequireConfirmedAccount = false;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 4;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+        })
+        .AddEntityFrameworkStores<ClinicDbContext>()
+        .AddDefaultUI()
+        .AddDefaultTokenProviders();
+
+    // Rejestracja kontrolerów oraz dodanie globalnego filtra wyjątków
+    builder.Services.AddControllersWithViews(options =>
+    {
+        options.Filters.Add<GlobalExceptionFilter>();
+    });
+
+    builder.Services.AddRazorPages();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+        app.UseHttpsRedirection();
+    }
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapStaticAssets();
+
+    app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}")
+        .WithStaticAssets();
+
+    app.MapRazorPages();
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<ClinicDbContext>();
+
+            await context.Database.MigrateAsync();
+
+            var config = services.GetRequiredService<IConfiguration>();
+            await DataSeeder.SeedRolesAndAdminAsync(services, config);
+        }
+        catch (Exception ex)
+        {
+            var diLogger = services.GetRequiredService<ILogger<Program>>();
+            diLogger.LogError(ex, "Błąd podczas seedowania bazy danych.");
+        }
+    }
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
-app.MapRazorPages();
-
-using (var scope = app.Services.CreateScope())
+catch (Exception ex)
 {
-    var services = scope.ServiceProvider;
-    try
-    {
-        var config = services.GetRequiredService<IConfiguration>();
-        await DataSeeder.SeedRolesAndAdminAsync(services, config);
-    }
-    catch (Exception ex)
-    {
-        // TODO: podpięcie NLog (US#2)
-        Console.WriteLine($"Błąd podczas seedowania bazy danych: {ex.Message}");
-    }
+    logger.Error(ex, "Aplikacja zakończyła działanie z powodu nieobsłużonego wyjątku.");
+    throw;
 }
-
-app.Run();
+finally
+{
+    NLog.LogManager.Shutdown();
+}
